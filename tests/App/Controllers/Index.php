46e9edddf8025eb333de5f2799015e483d6e85bb
Tom Flídr <tomflidr@gmail.com>
2018-05-09 17:01:19 +0200

    tests app

---
+++ tests/App/Controllers/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+class Index extends \MvcCore\Controller
+{
+	public function Init() {
+		date_default_timezone_set('Europe/Prague');
+	}
+    public function IndexAction () {
+		$cfg = array(
+			'driver'	=> 'mysql',
+			'host'		=> 'localhost',
+			'user'		=> 'root',
+			'password'	=> '1234',
+			'database'	=> 'tests'
+		);
+		if ($_SERVER['SERVER_NAME'] == 'dev.tests.tomflidr.cz') {
+			$cfg['user'] = 'tests';
+			$cfg['password'] = '4g%88v§kgaYJH@tVw';
+		}
+		$db = \MvcCore\Model::GetDb($cfg);
+		$select = $db->prepare(
+			"SELECT * FROM langs_and_locales t"
+			//." WHERE win_strftime_support = 1"
+		);
+		$select->execute();
+		$data = $select->fetchAll(\PDO::FETCH_ASSOC);
+		foreach ($data as $key => $value) $data[$key] = (object) $value;
+
+
+		$this->view->data = $data;
+	}
+}
---
+++ tests/index.php
@@ -0,0 +1,7 @@
+<?php
+
+	@include_once('vendor/autoload.php');
+
+	// \MvcCore\Config::SetEnvironment('development');
+
+	\MvcCore\Application::GetInstance()->Run();
