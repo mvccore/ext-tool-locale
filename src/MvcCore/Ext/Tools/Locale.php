4d59d28b3655d45e1f42aa37dd1782740aba2e48
Tom Flídr <tomflidr@gmail.com>
2024-11-21 16:01:02 +0100

    release v5.3

--- src/MvcCore/Ext/Tools/Locale.php
+++ src/MvcCore/Ext/Tools/Locale.php
@@ -30,7 +30,7 @@ class Locale {
 	 * Comparison by PHP function `version_compare();`.
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.2';
+	const VERSION = '5.3.0';
 
 	/**
 	 * All possible language codes and names supported on windows platforms.
